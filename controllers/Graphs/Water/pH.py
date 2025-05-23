6530381894530210237baa7e01b2ff1036dee2f0
yisus050820 <enriquezjesus122@gmail.com>
2025-05-22 22:14:46 -0600

    style: Cambiar color del texto en la grafica

--- controllers/Graphs/Water/pH.py
+++ controllers/Graphs/Water/pH.py
@@ -8,7 +8,7 @@ class GraphHp(BaseGraph):
             title="",
             x_label="Tiempo (horas)",
             y_label="Valor (pH)",
-            line_color="#6CC6C1",
+            line_color="#045859",
             data_range=(0, 14),  # Rango completo de pH
             initial_data_length=24  # 24 horas de datos iniciales
         )
@@ -23,29 +23,35 @@ class GraphHp(BaseGraph):
     def custom_config(self):
         """Configuración adicional específica para pH"""
         # Aplicar estilos
-        style = {"color": "#333", "font-size": "11px"}
-        
+        style = {"color": "#045859", "font-size": "11px"}
+
         # Establecer fondo blanco
         self.setBackground("white")
-        
-        # Configurar etiquetas con el estilo deseado        self.getPlotItem().setLabel("left", "Escala de pH", **style)
+
+        # Configurar etiquetas con el estilo deseado
+        self.getPlotItem().setLabel("left", "Escala de pH", **style)
         self.getPlotItem().setLabel("bottom", "Tiempo (horas)", **style)
-        
+
+        # Configurar el color de los ticks (números) en ambos ejes
+        self.getPlotItem().getAxis("left").setPen(pg.mkPen(style["color"]))
+        self.getPlotItem().getAxis("bottom").setPen(pg.mkPen(style["color"]))
+
         # Añadir leyenda
         self.getPlotItem().addLegend()
-        
+
         # Mostrar cuadrícula
         self.getPlotItem().showGrid(x=True, y=True, alpha=0.3)
-        
+
         # Modificar el estilo de la línea para añadir símbolos
         pen = pg.mkPen(color="#6CC6C1", width=2)
         self.curve.setPen(pen)
         self.curve.setSymbol('o')
         self.curve.setSymbolSize(7)
         self.curve.setSymbolBrush("#6CC6C1")
-          # Configura los límites físicos (min/max permitidos)
+
+        # Configura los límites físicos (min/max permitidos)
         self.getViewBox().setLimits(yMin=0, yMax=14)
-        
+
     def updateHp(self, new_value: float):
         """Actualiza la gráfica con un nuevo valor de pH"""
         if not self.x_data:  # Si es el primer punto

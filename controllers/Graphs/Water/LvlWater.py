2b0bb6f31d38011029146fe07e16a6264339c514
Eduarduar <eduardoarcega1997@gmail.com>
2025-05-27 23:11:05 -0600

    fix: correccion de la grafica del nivel del agua

--- controllers/Graphs/Water/LvlWater.py
+++ controllers/Graphs/Water/LvlWater.py
@@ -7,20 +7,18 @@ class GraphLvlWater(QWidget):
         self.setup_ui()
 
     def setup_ui(self):
-        # Configuración del layout y del gráfico...
         self.plot_widget = pg.PlotWidget()
         self.plot_widget.setBackground("white")
         self.plot_widget.setLabel("left", "cm Nivel", color="#045859", size="11px")
         self.plot_widget.setLabel("bottom", "Tiempo (minutos)", color="#045859", size="11px")
         self.plot_widget.showGrid(x=True, y=True)
-        self.plot_widget.setYRange(0, 50)
         self.plot_widget.enableAutoRange(axis=pg.ViewBox.XAxis, enable=True)
+        self.plot_widget.enableAutoRange(axis=pg.ViewBox.YAxis, enable=True)
 
         pen = pg.mkPen(color=(57, 195, 239), width=1)
 
         self.level_data = []
         self.time_data = []
-
 
         self.level_line = self.plot_widget.plot(
             self.time_data,
@@ -36,21 +34,19 @@ class GraphLvlWater(QWidget):
         layout.addWidget(self.plot_widget)
 
     def update_data(self, new_value):
-        """
-        Agrega un nuevo dato de nivel de agua y actualiza la gráfica.
-        """
         next_time = self.time_data[-1] + 1 if self.time_data else 0
         self.time_data.append(next_time)
         self.level_data.append(new_value)
 
-        # Mantener solo los últimos 20 datos
         self.time_data = self.time_data[-20:]
         self.level_data = self.level_data[-20:]
 
         self.level_line.setData(self.time_data, self.level_data)
 
+        # Autoajuste manual del eje Y si desactivas autoRange
+        # min_y = min(self.level_data)
+        # max_y = max(self.level_data)
+        # self.plot_widget.setYRange(min_y - 5, max_y + 5)
+
     def updateLvlWater(self, new_value):
-        """
-        Método público para ser llamado desde WaterComponent.
-        """
         self.update_data(new_value)
